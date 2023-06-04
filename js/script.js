101b412e6d341d6cc77c348931fa738d88a2ddb9
ImWfak <borusovaleks@gmail.com>
2023-06-04 17:36:30 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -235,3 +235,177 @@ const factorial = (num) => {
 }
 
 showBasic();
+
+const convertLenght = () => {
+    var inputValue = parseFloat(document.getElementById("lenghtValueInput").value);
+    var inputMatric = document.getElementById("inputLenghtMatric").value;
+    var outputMatric = document.getElementById("outputLenghtMatric").value;
+    var matrics = {
+        "cm": {
+          "cm": 1,
+          "m": 0.01,
+          "km": 0.00001,
+          "in": 0.393701,
+          "ft": 0.0328084,
+          "yd": 0.0109361,
+          "mi": 0.00000621371
+        },
+        "m": {
+          "cm": 100,
+          "m": 1,
+          "km": 0.001,
+          "in": 39.3701,
+          "ft": 3.28084,
+          "yd": 1.09361,
+          "mi": 0.000621371
+        },
+        "km": {
+          "cm": 100000,
+          "m": 1000,
+          "km": 1,
+          "in": 39370.1,
+          "ft": 3280.84,
+          "yd": 1093.61,
+          "mi": 0.621371
+        },
+        "in": {
+          "cm": 2.54,
+          "m": 0.0254,
+          "km": 0.0000254,
+          "in": 1,
+          "ft": 0.0833333,
+          "yd": 0.0277778,
+          "mi": 0.000015783
+        },
+        "ft": {
+          "cm": 30.48,
+          "m": 0.3048,
+          "km": 0.0003048,
+          "in": 12,
+          "ft": 1,
+          "yd": 0.333333,
+          "mi": 0.000189394
+        },
+        "yd": {
+          "cm": 91.44,
+          "m": 0.9144,
+          "km": 0.0009144,
+          "in": 36,
+          "ft": 3,
+          "yd": 1,
+          "mi": 0.000568182
+        },
+        "mi": {
+          "cm": 160934,
+          "m": 1609.34,
+          "km": 1.60934,
+          "in": 63360,
+          "ft": 5280,
+          "yd": 1760,
+          "mi": 1
+        }
+      };
+    var result = inputValue * matrics[inputMatric][outputMatric];
+    document.getElementById("lenghtResultInput").value = result.toFixed(1);
+}
+const convertMass = () => {
+    var inputValue = parseFloat(document.getElementById("massValueInput").value);
+    var inputMatric = document.getElementById("inputMassMatric").value;
+    var outputMatric = document.getElementById("outputMassMatric").value;
+    var matrics = {
+        "mg": {
+          "mg": 1,
+          "g": 0.001,
+          "kg": 0.000001,
+          "lb": 0.00000220462,
+          "oz": 0.00003527396
+        },
+        "g": {
+          "mg": 1000,
+          "g": 1,
+          "kg": 0.001,
+          "lb": 0.00220462,
+          "oz": 0.03527396
+        },
+        "kg": {
+          "mg": 1000000,
+          "g": 1000,
+          "kg": 1,
+          "lb": 2.20462,
+          "oz": 35.27396
+        },
+        "lb": {
+          "mg": 453592.37,
+          "g": 453.59237,
+          "kg": 0.45359237,
+          "lb": 1,
+          "oz": 16
+        },
+        "oz": {
+          "mg": 28349.52313,
+          "g": 28.34952313,
+          "kg": 0.02834952313,
+          "lb": 0.0625,
+          "oz": 1
+        }
+      };
+    var result = inputValue * matrics[inputMatric][outputMatric];
+    document.getElementById("massResultInput").value = result.toFixed(1);
+}
+const convertArea = () => {
+    var inputValue = parseFloat(document.getElementById("areaValueInput").value);
+    var inputMatric = document.getElementById("inputAreaMatric").value;
+    var outputMatric = document.getElementById("outputAreaMatric").value;
+    var matrics = {
+        "sqcm": {
+          "sqcm": 1,
+          "sqm": 0.0001,
+          "ha": 0.0000001,
+          "sqft": 0.00107639,
+          "sqyd": 0.000119599,
+          "acre": 0.0000000247105
+        },
+        "sqm": {
+          "sqcm": 10000,
+          "sqm": 1,
+          "ha": 0.0001,
+          "sqft": 10.7639,
+          "sqyd": 1.19599,
+          "acre": 0.000247105
+        },
+        "ha": {
+          "sqcm": 100000000,
+          "sqm": 10000,
+          "ha": 1,
+          "sqft": 107639,
+          "sqyd": 11959.9,
+          "acre": 2.47105
+        },
+        "sqft": {
+          "sqcm": 929.0304,
+          "sqm": 0.09290304,
+          "ha": 0.0000092903,
+          "sqft": 1,
+          "sqyd": 0.111111,
+          "acre": 0.0000229568
+        },
+        "sqyd": {
+          "sqcm": 8361.2736,
+          "sqm": 0.83612736,
+          "ha": 0.0000836127,
+          "sqft": 9,
+          "sqyd": 1,
+          "acre": 0.000206612
+        },
+        "acre": {
+          "sqcm": 40468564.224,
+          "sqm": 4046.8564224,
+          "ha": 0.4046856422,
+          "sqft": 43560,
+          "sqyd": 4840,
+          "acre": 1
+        }
+      };
+    var result = inputValue * matrics[inputMatric][outputMatric];
+    document.getElementById("areaResultInput").value = result.toFixed(9);
+}
