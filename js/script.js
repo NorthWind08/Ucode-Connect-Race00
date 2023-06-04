0e63c905d18e40b27c6f5b30f2760c3a2f1c10ed
northwind08 <zseveryn.lipovetskyz@gmail.com>
2023-06-04 15:15:49 +0300

    new: modes; new: advanced mode; wip: advanced calc

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 let equationStr = '';
 let equation = '';
 
+
 const updateEq = () => {
+    if (equationStr === '') {
+        document.querySelector(".eq-input").innerHTML = 'Enter equation';
+        return;
+    }
     document.querySelector(".eq-input").innerHTML = equationStr;
 }
 
@@ -16,11 +21,14 @@ const clearEq = () => {
 const calcRes = () => {
     validateEq();
     let res = 0;
-    try {
+    with (Math) try {
         res = eval(equation);
     }
-    catch { }
-    
+    catch {
+        document.querySelector(".result").innerHTML = 'Error: invalid equation';
+        return;
+    }
+
     if (
         res % 1 !== 0 && 
         String(res).length > 16
@@ -44,61 +52,186 @@ const calcRes = () => {
             }
         }
     }
-    findPercent();
     document.querySelector('.result').innerHTML = res;
     updateEq();
 }
 
 const validateEq = () => {
     equation = equationStr;
-    equation.replace('^', '**');
+    parsePercent();
+    parseFactorial();
+    equation = equation.replace('^', '**');
+    equation = equation.replace('π', 'PI');
+    equation = equation.replace('√', 'sqrt');
+}
 
+const parsePercent = () => {
     for (let i = 0; i < equation.length; i++) {
         if (equation[i] === '%') {
             let numIdx = i - 1;
             while (!isNaN(equation[numIdx])) {
                 numIdx--;
             }
-            let numPercent = equation.slice(numIdx + 1, i);
-            console.log(`i:${i}; numIdx:${numIdx}`);
-            console.log(numPercent);
-            return numPercent;
+            let numPercent = equation.slice(numIdx + 1, i);            
+            if (
+                equation[numIdx] === '*' ||
+                equation[numIdx] === '/' ||
+                equation[numIdx] === '^'
+            ) {
+                equation = equation.replace(numPercent + '%', String(Number(numPercent) / 100));
+            }
+            else {
+                let secondNum = numIdx - 1;
+                for (let i = numIdx - 1; i >= 0; i--) {
+                    if(isNaN(equation[i]) || i == 0) {
+                        secondNum = i;
+                        break;
+                    }
+                }
+                equation = equation.replace(
+                    numPercent + '%',
+                    String(Number(equation.slice(secondNum, numIdx)) * Number(numPercent) / 100)
+                );
+            }
+        }
+    }
+}
+
+const parseFactorial = () => {
+    for (let i = 0; i < equation.length; i++) {
+        if (equation[i] === '!') {
+            if (!isNaN(equation[i - 1])) {
+                let numStart = i - 1;
+                for (;numStart >= 0; numStart--) {
+                    if (numStart === 0 || isNaN(numStart)) {
+                        break;
+                    }
+                }
+                equation = equation.replace(
+                    equation.slice(numStart, i+1),
+                    `factorial(${equation.slice(numStart, i)})`
+                );
+            }
         }
     }
 }
 
 const operatorPress = (char) => {
-    if (isNaN(char) && isNaN(equationStr[equationStr.length - 1])) {
-        equationStr = equationStr.replace(equationStr[equationStr.length - 1], char);
-        updateEq();
-        return;
-    }
+
+    // if (char === 'sin') {
+    //     equationStr += char + '(';
+    //     updateEq();
+    //     return;
+    // }
+
+    // if (char === 'cos') {
+    //     equationStr += char + '(';
+    //     updateEq();
+    //     return;
+    // }
+
+    // if (char === 'tan') {
+    //     equationStr += char + '(';
+    //     updateEq();
+    //     return;
+    // }
+
+    // if (char === 'π') {
+    //     equationStr += char;
+    //     updateEq();
+    //     return;
+    // }
+
+    // if (char === '√') {
+    //     equationStr += char + '(';
+    //     updateEq();
+    //     return;
+    // }
+
+
+    // if (char === '-' && equationStr === '') {
+    //     equationStr += char;
+    //     updateEq();
+    //     return;
+    // }
+
+    // if (isNaN(char) && equationStr === '') {
+    //     return;
+    // }
+
+    // if (isNaN(char) && isNaN(equationStr[equationStr.length - 1])) {
+    //     equationStr = equationStr.replace(equationStr[equationStr.length - 1], char);
+    //     updateEq();
+    //     return;
+    // }
 
     equationStr += char;
     updateEq();
 }
 
-const hide = () => {
-    document.getElementById('standard').style.display = "none";
+const backSpace = () => {
+    // console.log("backspace called");
+    // console.log(equationStr.slice(0, equationStr.length - 1));
+    equationStr = equationStr.slice(0, equationStr.length - 1);
+    updateEq();
 }
 
-const findPercent = () => {
-    numPercent = validateEq();
-    let numBeforePercent = '';
-        for (let i = equation.indexOf(numPercent[0]) - 2; i >= 0; i--) {
-            if (isNaN(equation[i])) {
-                break;
-            }
-            numBeforePercent += equation[i];
-        }
-        numBeforePercent = reverseString(numBeforePercent);
-        let result = (numBeforePercent / 100) * numPercent;
-        console.log(`percent ${numPercent}`);
-        console.log(`nbp ${numBeforePercent}`);
-        console.log(`result: ${result}`);
-        equation.replace('%', '');
-        equation.replace(numPercent, result);
+const showBasic = () => {
+    document.getElementById("basic").style.display = 'flex'
+    document.getElementById("converter").style.display = 'none'
+
+    document.getElementById("sin").style.display = 'none';
+    document.getElementById("cos").style.display = 'none';
+    document.getElementById("tan").style.display = 'none';
+    document.getElementById("pi").style.display = 'none';
+    document.getElementById("open-bracket").style.display = 'none';
+    document.getElementById("close-bracket").style.display = 'none';
+    document.getElementById("factorial").style.display = 'none';
+    document.getElementById("square-root").style.display = 'none';
+
+    document.getElementById("basic-btn").style.backgroundColor = 'var(--background)';
+    document.getElementById("advanced-btn").style.backgroundColor = 'var(--mode)';
+    document.getElementById("converter-btn").style.backgroundColor = 'var(--mode)';
 }
-const reverseString = (str) => {
-    return [...str].reverse().join('');
+
+const showAdvanced = () => {
+    document.getElementById("basic").style.display = 'flex'
+    document.getElementById("converter").style.display = 'none'
+
+    document.getElementById("sin").style.display = 'initial';
+    document.getElementById("cos").style.display = 'initial';
+    document.getElementById("tan").style.display = 'initial';
+    document.getElementById("pi").style.display = 'initial';
+    document.getElementById("open-bracket").style.display = 'initial';
+    document.getElementById("close-bracket").style.display = 'initial';
+    document.getElementById("factorial").style.display = 'initial';
+    document.getElementById("square-root").style.display = 'initial';
+
+
+    document.getElementById("basic-btn").style.backgroundColor = 'var(--mode)';
+    document.getElementById("advanced-btn").style.backgroundColor = 'var(--background)';
+    document.getElementById("converter-btn").style.backgroundColor = 'var(--mode)';
+
 }
+
+const showConverter = () => {
+    document.getElementById("basic").style.display = 'none'
+    document.getElementById("converter").style.display = 'flex'
+
+    document.getElementById("basic-btn").style.backgroundColor = 'var(--mode)';
+    document.getElementById("advanced-btn").style.backgroundColor = 'var(--mode)';
+    document.getElementById("converter-btn").style.backgroundColor = 'var(--background)';
+
+}
+
+const factorial = (num) => {
+    if (num < 0) {
+        return -1;
+    }
+    if (num === 0) {
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+
+showBasic();
